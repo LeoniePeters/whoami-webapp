085ce1ed5e063b6fe3f12145c090301b331ccf3d
LeoniePeters <leoniepeters@gmail.com>
2019-03-21 23:03:02 +0100

    Change background and create components

---
+++ src/components/IntroductionPage.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import './IntroductionPage.css';
+
+class IntroductionPage extends Component {
+//   state = {
+//       display: 'home'
+//     }
+
+//   handleOnClick = () => {
+//     this.setState({display: 'introduction'})
+//   }
+
+  render() {
+    return (
+      <div className='intro'>
+      Hoi
+        
+      </div>
+    );
+  }
+}
+
+export default IntroductionPage;
