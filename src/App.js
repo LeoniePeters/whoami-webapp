23b5bf56f0f1cd36af37a633dd4ea612f0c352f6
LeoniePeters <leoniepeters@gmail.com>
2019-03-21 16:54:41 +0100

    Make background hoverable

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import star1 from './images/star-r2b3.png'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <p>'Hello'</p>
+      <img src={star1} alt='star1' id="star1" />
 
       </div>
     );
